7ddd064a2449fe92ad6688624a3aa401393923aa
AndrejHronco <andrejhronco@gmail.com>
2014-12-02 00:06:22 -0800

    Noise, Panner
    noise and panner functions added

--- scripts/app.js
+++ scripts/app.js
@@ -1,50 +1,23 @@
 (function(){
 	var AudioContext = window.AudioContext || window.webkitAudioContext;
 	var audioCtx = new AudioContext();
-	// console.log('sampleRate: ', audioCtx.sampleRate);
+	var W = window.innerWidth;
+	var H = window.innerHeight;
+	
 	// start
-	//init();
+	init();
 
 	function init(){
-		var brownNoise = audioCtx.createBrownNoise();
-		var brownGain = audioCtx.createGain();
-		brownGain.gain.value = 0.1;
-		brownNoise.connect(brownGain);
+		var pan1 = panner(audioCtx, {x: W/2, y: H/2, z: 100});
+		var pan2 = panner(audioCtx, {x: 0, y: H, z: 10});
+		var pan3 = panner(audioCtx, {x: W, y: 0, z: 100});
 
-		var lfo = audioCtx.createOscillator();
-		lfo.frequency.value = Math.random();
-		var lfoGain = audioCtx.createGain();
-		lfoGain.gain.value = 1;
-
-		lfo.start(0);
-		lfo.connect(lfoGain);
-		lfoGain.connect(brownGain.gain);
-		brownGain.connect(audioCtx.destination);
-
-		var pinkNoise = audioCtx.createPinkNoise();
-		var pinkGain = audioCtx.createGain();
-		var pinkFilter = audioCtx.createBiquadFilter();
-		pinkGain.gain.value = 100;
-		pinkFilter.frequency.value = 1.618;
-		pinkNoise.connect(pinkFilter);
-		pinkFilter.connect(pinkGain);
-
-		var saw = audioCtx.createOscillator();
-		saw.type = saw.SAWTOOTH;
-		saw.frequency.value = 440.0;
-		var sawGain = audioCtx.createGain();
-		sawGain.gain.value = 0.2;
-
-		saw.start(0);
-		saw.connect(sawGain);
-		pinkGain.connect(saw.frequency);
-		sawGain.connect(audioCtx.destination);
+		noise(audioCtx,"brown", pan1);
+		noise(audioCtx,"white", pan2);
+		noise(audioCtx,"pink", pan3);
 	}
 
-
-
 /* Audio Functions */
-	//create more of these audio functions to choose from
 	function Modulator (type, freq, gain) {
 	  this.modulator = audioCtx.createOscillator();
 	  this.gain = audioCtx.createGain();
@@ -56,6 +29,89 @@
 	  console.log('Mod:', type, freq, gain);
 	}
 
+	function noise(context, type, pan){
+		//type = {type: "brown", gain: n}
+		var n, ng, lfo,lfog;
+		switch(type.toLowerCase()){
+			case "brown":
+				n = context.createBrownNoise();
+				break;
+			case "white":
+				n = context.createWhiteNoise();
+				break;
+			case "pink":
+				n = context.createPinkNoise();
+			default:
+				break;
+		}
+		ng = context.createGain();
+
+		lfo = context.createOscillator();
+		lfo.frequency.value = Math.random() * .5; //controls the crazy
+		lfog = context.createGain();
+		lfog.gain.value = Math.random() * 100;
+
+		lfo.start(0);
+		lfo.connect(lfog);
+		lfog.connect(ng.gain);
+
+		if(pan){ //gain is low when pan is enabled
+			n.connect(pan);
+			pan.connect(ng);
+			ng.gain.value = 10;
+			ng.connect(context.destination);
+		} else{
+			n.connect(ng);
+			ng.gain.value = 1;
+			ng.connect(context.destination);	
+		}
+		
+	}
+
+	function panner(context, position, velocity){
+		//position = {x: x, y: y, z: z}
+		//velocity = {x: x, y: y, z: z}
+		var panner, listener; 
+		panner = context.createPanner();
+		listener = context.listener;
+
+		listener.dopplerFactor = 1;
+		listener.speedOfSound = 343.3;
+		listener.setOrientation(0,0,-1,0,1,0);
+		listener.setPosition(W/2, H/2, 300);
+
+		panner.setOrientation(1,0,0);
+		panner.setPosition(position.x, position.y, position.z);
+		panner.setVelocity(100,0,100);
+
+		return panner;
+	}
+
+	function filter(context, type, freq){
+	/*
+			var pinkNoise = audioCtx.createPinkNoise();
+			var pinkGain = audioCtx.createGain();
+			var pinkFilter = audioCtx.createBiquadFilter();
+			pinkGain.gain.value = 100;
+			pinkFilter.frequency.value = 1.618;
+			pinkNoise.connect(pinkFilter);
+			pinkFilter.connect(pinkGain);
+
+			var saw = audioCtx.createOscillator();
+			// type
+			saw.type = saw.SAWTOOTH;
+			//freq
+			saw.frequency.value = 150.0;
+			var sawGain = audioCtx.createGain();
+			sawGain.gain.value = 0.2;
+
+			saw.start(0);
+			saw.connect(sawGain);
+			pinkGain.connect(saw.frequency);
+			sawGain.connect(audioCtx.destination);
+			*/
+			// return sawGain;
+	}
 /* Utilities */
 	function time(){
 		var d = new Date(),
