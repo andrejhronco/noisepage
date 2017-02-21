342883c511da93ed2ec579a207c9cfade3f21f3d
Andrej KMI <andrej@keithmcmillen.com>
2017-02-20 17:45:33 -0800

    Presets, Renaming, URL preset
    Store presets locally
    renamed gui items for recall
    setting preset in url

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,4 @@
 /*
-	on load: create random amount of noise / panner sets with random noise type value
-	change frequency values via mouse / midi / data.gui
-	populate dat.gui based off of random creation
-
-	'refresh all'f function
-
-	noiseControl is where dat.gui is created and set
-
-*/
-
-
-// set init lfo/g lets to 0
-// makeNoise() is init, called on load and then onChange, contains all noise() instances
-// on gui change set lfo/g values and pass them to makeNoise()/noise()
-// might need a func on gui change that returns all values, pass these into noise() and call makeNoise()
-/*
 var first = true
 // var maxChannelCount = Tone.context.destination.maxChannelCount;
 var maxChannelCount = 8
@@ -62,34 +46,41 @@ setInterval(() => {
 }, 5000)
 */
 
-// add gain to each noise
-// add gui to mod frequency
+/* TODO
+	save presets,
+	load presets,
+	load preset from URL
+	better visuals : https://github.com/fluuuid/labs/blob/master/lines/Line.js
+*/
+let state = {};
+const waveTypes = ['sine', 'square', 'sawtooth', 'triangle'],
+			noiseTypes = ['pink', 'pink', 'white', 'brown', 'brown'],
+			letters = ['A', 'B', 'C', 'D', 'E', 'F'];
 
 (function(Tone){
-	const waveTypes = ['sine', 'square', 'sawtooth', 'triangle'],
-				noiseTypes = ['pink', 'pink', 'brown', 'white'],
-				letters = ['A', 'B', 'C', 'D', 'E', 'F']
-
 	let preset = [{
 			type: 'brown',
-			volume: random(),
-			panRate: random([1,10]),
-			modRate: random([1,10])
+			volume: [-30,0],
+			panRate: [1,10],
+			modRate: [1,1000]
 		},{
 			type: 'pink',
-			volume: random(),
-			panRate: random([1,10]),
-			modRate: random([1,10])
+			volume: [-30,0],
+			panRate: [1,10],
+			modRate: [1,1000]
 		},{
 			type: 'white',
-			volume: random(),
-			panRate: random([1,10]),
-			modRate: random([1,10])
+			volume: [-30,0],
+			panRate: [1,10],
+			modRate: [1,1000]
 	}]
 
-	let interval = 10000, intervalID
+	let interval = 10, intervalID
 
 	let noiseSets = makeNoiseSet(preset)
+
+	// save initial state
+	setState(noiseSets.sets)
 
 	// DAT GUI
 	let gui = new dat.GUI(),
@@ -103,18 +94,27 @@ setInterval(() => {
 		if(active){
 			intervalID = setInterval(() => {
 				randomizer()
-			}, controls.interval)
+			}, controls.interval * 1000)
 			console.log('turning on randomizer')
 		} else {
 			console.log('turning off randomizer')
 		}
 	}).listen()
-	gui.add(controls, 'interval', 1000, 60000)
+	gui.add(controls, 'interval', 1, 60)
 
 	gui.add(controls, 'start')
 	gui.add(controls, 'stop')
 
+	gui.add(controls, 'presetName')
+	gui.add(controls, 'save')
+	gui.add(controls, 'load', getStoredPresets()).onChange(value => {
+		let preset = localStorage.getObject(value)
+		setValues(preset, noiseSets.sets, controls)
+	}).listen()
+
 // gui.remember(controls);
+
+console.log('controls', controls)
 
 function createControls(noiseSets){
 	let controls = {
@@ -125,13 +125,20 @@ function createControls(noiseSets){
 		stop : () => {
 			if(intervalID) clearInterval(intervalID)
 			noiseSets.stopAll()
-		}
+		},
+		presetName: 'preset',
+		save: function() {
+			saveStateToLocal(this.presetName)
+		},
+		load: ['']
 	}
 
 	noiseSets.sets.forEach((set, i) => {
-		controls['volume'+ letters[i]] = noiseSets.sets[i].gain.gain.value
-		controls['panRate'+ letters[i]] = noiseSets.sets[i].pan.frequency.value
-		controls['modRate'+ letters[i]] = noiseSets.sets[i].mod.frequency.value
+		controls['noise.type.'+ letters[i]] = set.noise.type
+		controls['pan.type.'+ letters[i]] = set.pan.type
+		controls['volume.'+ letters[i]] = set.volume.volume.value
+		controls['pan.frequency.'+ letters[i]] = set.pan.frequency.value
+		controls['mod.frequency.'+ letters[i]] = set.mod.frequency.value
 	})
 
 	return controls
@@ -139,88 +146,94 @@ function createControls(noiseSets){
 
 function addGUI(controls, noiseSets){
 	noiseSets.sets.map((set, i) => {
-		let noise = gui.addFolder('Noise ' + letters[i] + ' (' + set.noise.type + ')')
+		let noise = gui.addFolder('Noise ' + letters[i])
 		noise.open()
 
-		noise.add(controls, 'volume'+letters[i], 0.0, 1.0).step(0.1).onChange((value) => {
-			set.gain.gain.setValueAtTime(value, 0)
+		noise.add(controls, 'noise.type.'+letters[i], [ 'pink', 'brown', 'white' ]).onChange((value) => {
+			set.noise.type = value
 		}).listen();
 
-		noise.add(controls, 'panRate'+letters[i], 1, 10).onChange((value) => {
+		noise.add(controls, 'pan.type.'+letters[i], waveTypes).onChange((value) => {
+			set.pan.type = value
+		}).listen();
+
+		noise.add(controls, 'volume.'+letters[i], set.volume.range[0], set.volume.range[1]).onChange((value) => {
+			set.volume.volume.setValueAtTime(value, 0)
+		}).listen();
+
+		noise.add(controls, 'pan.frequency.'+letters[i], set.pan.range[0], set.pan.range[1]).onChange((value) => {
 			set.pan.frequency.setValueAtTime(value, 0)
 		}).listen();
 
-		noise.add(controls, 'modRate'+letters[i], 1, 500).onChange((value) => {
+		noise.add(controls, 'mod.frequency.'+letters[i], set.mod.range[0], set.mod.range[1]).onChange((value) => {
 			set.mod.frequency.setValueAtTime(value, 0)
 		}).listen();
 	})
 }
 
 function randomizer(){
-	interval = controls.interval
+	interval = controls.interval * 1000
 	noiseSets.sets.map( (noiseSet, i) => {
-		console.log('\nnoise'+letters[i])
+		// console.log('noiseSet', noiseSet)
 		noiseSet.noise.type = noiseTypes[random([0,noiseTypes.length-1], true)]
 		noiseSet.pan.type = waveTypes[random([0,waveTypes.length-1], true)]
 
+		// update gui
+		controls['noise.type.'+letters[i]] = noiseSet.noise.type
+
+		// update gui
+		controls['pan.type.'+letters[i]] = noiseSet.pan.type
+
 		switch(random([0,2], true)){
 			case 0:
-				noiseSet.pan.set({'frequency': random([1,10])}, interval / random([1000,interval]))
+				noiseSet.pan.set({'frequency': random([noiseSet.pan.range[0],noiseSet.pan.range[1]])}, interval / random([1000,interval]))
 				break;
 			case 1:
-				noiseSet.pan.set({'frequency': random([1,10])}, interval)
+				noiseSet.pan.set({'frequency': random([noiseSet.pan.range[0],noiseSet.pan.range[1]])}, interval)
 				break;
 			case 2:
-				noiseSet.pan.frequency.value = random([1,10])
+				noiseSet.pan.frequency.value = random([noiseSet.pan.range[0],noiseSet.pan.range[1]])
 		}
 		// update gui
-		controls['panRate'+letters[i]] = noiseSet.pan.frequency.value
-
-		console.log('pan freq', noiseSet.pan.frequency.value)
+		controls['pan.frequency.'+letters[i]] = noiseSet.pan.frequency.value
 
 		noiseSets.sets.map( noiseSet => noiseSet.pan.depth.value = random(1))
-/*
+
 		switch(random([0,2], true)){
 			case 0:
-				noiseSet.noise.set({'volume': random([-50, 0])}, interval / random([1000,interval]))
+				noiseSet.volume.set({'volume': random([noiseSet.volume.range[0],noiseSet.volume.range[1]])}, interval / random([1000,interval]))
 				break;
 			case 1:
-				noiseSet.noise.set({'volume': random([-50, 0])}, interval)
+				noiseSet.volume.set({'volume': random([noiseSet.volume.range[0],noiseSet.volume.range[1]])}, interval)
 				break;
 			case 2:
-				noiseSet.noise.volume.value = random([-50, 0])
+				noiseSet.volume.volume.value = random([noiseSet.volume.range[0],noiseSet.volume.range[1]])
 		}
 
 		// update gui
-		controls['volume'+letters[i]] = noiseSet.noise.volume.value
+		controls['volume.'+letters[i]] = noiseSet.volume.volume.value
 
-		console.log('noise vol', noiseSet.noise.volume.value)
-*/
 		switch(random([0,2], true)){
 			case 0:
-				noiseSet.mod.set({'frequency': random([1,500]), 'volume': random([-50,0])}, interval / random([1000,interval]))
+				noiseSet.mod.set({'frequency': random([noiseSet.mod.range[0],noiseSet.mod.range[1]]), 'volume': random([-10,0])}, interval / random([1000,interval]))
 				break;
 			case 1:
-				noiseSet.mod.set({'frequency': random([1,500]), 'volume': random([-50,0])}, interval)
+				noiseSet.mod.set({'frequency': random([noiseSet.mod.range[0],noiseSet.mod.range[1]]), 'volume': random([-10,0])}, interval)
 				break;
 			case 2:
-				noiseSet.mod.frequency.value = random([1,500])
-				noiseSet.mod.volume.value = random([-5,0])
+				noiseSet.mod.frequency.value = random([noiseSet.mod.range[0],noiseSet.mod.range[1]])
+				noiseSet.mod.volume.value = random([-10,0])
 		}
 
 		// update gui
-		controls['modRate'+letters[i]] = noiseSet.mod.frequency.value
-
-		console.log('mod freq', noiseSet.mod.frequency.value)
-		console.log('mod volume', noiseSet.mod.volume.value)
+		controls['mod.frequency.'+letters[i]] = noiseSet.mod.frequency.value
 
 		noiseSet.mod.frequency.type = waveTypes[random([0,waveTypes.length-1], true)]
 
-		// noiseSet.pitchShift.value = random([1,12], true)
-		let partials = Array.from({length: random([1,5], true)}).map(item => random())
-		if(random([0,1], true)) noiseSet.mod.partials = partials
+		if(random([0,1], true)) noiseSet.mod.partials = Array.from({length: random([1,5], true)}).map(item => random())
 
 	})
+	setState(noiseSets.sets)
 }
 })(Tone);
 
@@ -242,37 +255,110 @@ function makeNoiseSet(preset){
 
 function makeNoise(settings) {
 	let noise = new Tone.Noise(settings.type),
-			pan = new Tone.AutoPanner(settings.panRate),
-			mod = new Tone.Oscillator(settings.modRate),
-			gain = new Tone.Gain(settings.volume)
-			// pitchShift = new Tone.PitchShift(random(12))
+			pan = new Tone.AutoPanner(random(settings.panRate)),
+			mod = new Tone.Oscillator(random(settings.modRate)),
+			volume = new Tone.Volume(random(settings.volume))
+
+	// set ranges on nodes
+	pan.range = settings.panRate
+	mod.range = settings.modRate
+	volume.range = settings.volume
 
 	mod.connect(noise.volume)
-	// mod.connect(pan.frequency)
-	// noise.chain(pan, pitchShift, Tone.Master)
-	noise.chain(gain, pan, Tone.Master)
+	noise.chain(volume, pan, Tone.Master)
 
 	return {
 		noise: noise,
-		gain: gain,
+		volume: volume,
 		pan: pan,
 		mod: mod,
-		// pitchShift: pitchShift,
+
 		start: () => {
-			pan.start()
 			noise.start()
+			pan.start()
 			mod.start()
 
 			return this
 		},
 		stop: () => {
-			pan.stop()
 			noise.stop()
+			pan.stop()
 			mod.stop()
 
 			return this
 		}
 	}
+}
+
+// preset saving / setting
+function setState(sets){
+	sets.map((set, i) => {
+		state[i + '_' + 'noise.type'] = set.noise.type
+		state[i + '_' + 'pan.type'] = set.pan.type
+		state[i + '_' + 'volume.volume.value'] = set.volume.volume.value
+		state[i + '_' + 'pan.frequency.value'] = set.pan.frequency.value
+		state[i + '_' + 'mod.frequency.value'] = set.mod.frequency.value
+		state[i + '_' + 'mod.volume.value'] = set.mod.volume.value
+	})
+
+	addPresetToURL(state)
+	printValues(state)
+}
+
+function setValues(state, sets, controls){
+	Object.keys(state).map(item => {
+		let index = item.split('_')[0], paramList = item.slice(2).split('.')
+
+		if(paramList[2]){
+			sets[index][paramList[0]][paramList[1]][paramList[2]] = state[item]
+		} else {
+			sets[index][paramList[0]][paramList[1]] = state[item]
+		}
+
+		controls[paramList[0] + '.' + paramList[1] + '.' + letters[index]] = state[item]
+
+	console.log('::', ...paramList, '<>', state[item])
+	})
+}
+
+function addPresetToURL(state) {
+	history.pushState(null, null, '#' + encodeURIComponent(JSON.stringify(state)));
+}
+
+function readPresetFromURL(){
+	return JSON.parse(decodeURIComponent(document.location.hash.slice(1)));
+}
+
+function saveTinyURL(state) {
+	var url = document.location.protocol + "//" + document.location.host + document.location.pathname + "#" + encodeURIComponent(JSON.stringify(state));
+	url = encodeURIComponent(url);
+
+	window.open("http://tinyurl.com/api-create.php?url=" + url, "_blank", "width=300,height=100");
+}
+
+function saveStateToLocal(name){
+	localStorage.setObject(name, state)
+}
+
+function getStoredPresets() {
+	return Object.keys(localStorage)
+}
+
+function printValues(state){
+	console.log('\n')
+	Object.keys(state).forEach( item => {
+		let value = (isNaN(state[item])) ? state[item] : Number(state[item])
+
+		console.log(item + ': ', value)
+	})
+}
+
+Storage.prototype.setObject = function(key, value) {
+  this.setItem(key, JSON.stringify(value));
+}
+
+Storage.prototype.getObject = function(key) {
+  return JSON.parse(this.getItem(key));
 }
 
 /*
@@ -296,16 +382,6 @@ function makeNoise(settings) {
 	}
 */
 
-/*
-	function print_data(data){
-		let info = document.createElement('div');
-		for(p in data){
-			info.innerHTML += p + ' : ' + data[p] + '<br>';
-		}
-		document.body.appendChild(info);
-	}
-*/
-
 function random(range = 1, int = false){
 	let num
 
@@ -324,15 +400,3 @@ function random(range = 1, int = false){
 	}
 	return num
 }
-
-/*
-	// ios enable sound output
-	window.addEventListener('touchstart', function(){
-		//create empty buffer
-		let buffer = audioCtx.createBuffer(1, 1, 22050);
-		let source = audioCtx.createBufferSource();
-		source.buffer = buffer;
-		source.connect(audioCtx.destination);
-		source.start(0);
-	}, false);
-*/
